015e0e253c5db739b3923420a9fa7820dab7356c
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-17 20:52:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
   end
   
   def self.create(attributes)
-    	student = Student.new(attributes[name], attributes[grade])
+    	student = Student.new(attributes[:name], attributes[:grade])
     	student.save
     	student
   end
